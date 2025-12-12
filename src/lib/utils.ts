de468b59bebe1e2cb7940250a683f51ae9c95d7c
iiivanpopov <iiivanpopov999@gmail.com>
2025-12-12 20:15:19 +0200

    ⚡️  chore(frontend): change tw-merge=>tailwind-merge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import type { ClassValue } from 'clsx'
 import { clsx } from 'clsx'
-import { twMerge } from 'tw-merge'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...classes: ClassValue[]) {
   return twMerge(clsx(...classes))
